aa0d7cb95f5bfb524e0a73ef8887f8b55bdd0ed4
Doreen162 <doreenligman17@gmail.com>
2020-10-27 14:48:40 +0200

    Add router

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,15 @@
 import addBlogs from './components/addBlog.vue';
 import showBlogs from './components/showBlogs.vue';
 import listBlogs from './components/listBlogs.vue';
+import header from './components/header.vue';
+
 
 export default {
     components: {
         'add-blogs': addBlogs,
         'show-blogs': showBlogs,
-        'list-blogs': listBlogs 
+        'list-blogs': listBlogs,
+        'app-header': header, 
 
     },
     data () {
